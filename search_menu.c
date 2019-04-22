b09755ae02ab8e3fc877c83268a92cb7c0b0e228
Mohsin I Amin <48616524+MohsinIAmin@users.noreply.github.com>
2019-04-22 22:22:19 +0600

    Update search_menu.c

--- search_menu.c
+++ search_menu.c
@@ -30,7 +30,21 @@ void search_menu()
     printf("\t\t#ROLL[BATCH GPA UNDER](press P)\n");
     printf("\t\t#ROLL[BATCH GPA EXACT](press Q)\n");
     printf("\t\t#ROLL[INDIVUDIAL GPA OVER](press R)\n");
-    printf("\t\t#ROLL[INDIVIDUAL GPA UNDER](press S)\n\t\t#");
+    printf("\t\t#ROLL[INDIVIDUAL GPA UNDER](press S)\n");
+    printf("\t\t#ROLL,SUBJECT[MARK](press T)\n");
+    printf("\t\t#ROLL[OVER MARK](press U)\n");
+    printf("\t\t#ROLL[UNDER MARK](press V)\n");
+    printf("\t\t#ROLL[OVER GPA](press W)\n");
+    printf("\t\t#ROLL[UNDER GPA](press X)\n");
+    printf("\t\t#ROLL,SUBJECT(BATCH)[MARK](press Y)\n");
+    printf("\t\t#ROLL(BATCH)[OVER MARK](press Z)\n");
+    printf("\t\t#ROLL(BATCH)[UNDER MARK](press a)\n");
+    printf("\t\t#ROLL(BATCH)[OVER GPA](press b)\n");
+    printf("\t\t#ROLL(BATCH)[UNDER GPA](press c)\n");
+    printf("\t\t#ROLL,SUBJECT(BATCH)[OVER MARK](press d)\n");
+    printf("\t\t#ROLL,SUBJECT(BATCH)[UNDER MARK](press e)\n");
+    printf("\t\t#ROLL,SUBJECT(BATCH)[OVER GPA](press f)\n");
+    printf("\t\t#ROLL,SUBJECT(BATCH)[UNDER GPA](press g)\n\t\t#");
     fflush(stdin);
     temp=getchar();
     if(temp=='1'||temp=='2'||temp=='3') {
@@ -60,20 +74,20 @@ void search_menu()
     } else if(temp=='E') {
         printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
         sarch(temp);
-    }else if(temp=='F') {
+    } else if(temp=='F') {
         printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
         sarch(temp);
-    }else if(temp=='G') {
+    } else if(temp=='G') {
         printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
         sarch(temp);
-    }else if(temp=='H') {
+    } else if(temp=='H') {
         printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
         sarch(temp);
     } else if(temp=='I') {
         sarch(temp);
     }  else if(temp=='J') {
         sarch(temp);
-    }else if(temp=='K') {
+    } else if(temp=='K') {
         sarch(temp);
     } else if(temp=='L') {
         printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
@@ -83,18 +97,55 @@ void search_menu()
     } else if(temp=='N') {
         printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
         sarch(temp);
-    }else if(temp=='O') {
+    } else if(temp=='O') {
         printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
         sarch(temp);
-    }else if(temp=='P') {
+    } else if(temp=='P') {
         printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
         sarch(temp);
-    }else if(temp=='Q') {
+    } else if(temp=='Q') {
         printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
         sarch(temp);
     } else if(temp=='R') {
         sarch(temp);
     }  else if(temp=='S') {
+        sarch(temp);
+    } else if(temp=='T') {
+        sarch(temp);
+    } else if(temp=='U') {
+        sarch(temp);
+    } else if(temp=='V') {
+        sarch(temp);
+    } else if(temp=='W') {
+        sarch(temp);
+    } else if(temp=='X') {
+        sarch(temp);
+    } else if(temp=='Y') {
+        printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
+        sarch(temp);
+    } else if(temp=='Z') {
+        printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
+        sarch(temp);
+    } else if(temp=='a') {
+        printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
+        sarch(temp);
+    } else if(temp=='b') {
+        printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
+        sarch(temp);
+    } else if(temp=='c') {
+        printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
+        sarch(temp);
+    }else if(temp=='d') {
+        printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
+        sarch(temp);
+    } else if(temp=='e') {
+        printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
+        sarch(temp);
+    } else if(temp=='f') {
+        printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
+        sarch(temp);
+    } else if(temp=='g') {
+        printf("\t\t#Just Write BSSE(batch_number)i.e. BSSE01,BSSE02 and so on#\n");
         sarch(temp);
     }
     return;
