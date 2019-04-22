681b259fa5e852f80d645b372a167fb5b2ae330c
Mohsin I Amin <48616524+MohsinIAmin@users.noreply.github.com>
2019-04-22 22:29:19 +0600

    Update MO.c

--- MO.c
+++ MO.c
@@ -1,9 +1,10 @@
+//Run on Windows
 #include<stdlib.h>
 #include<stdio.h>
 int main(){
 
 system("gcc calculate_gpa.c data.h function.h input_marks.c input_profile.c main.c marks_input_menu.c menu.c print_cgpa.c print_gpa.c print_result.c search_1.c search_2.c search_menu.c searching.c show_raw_data.c subject_name.c update_cgpa.c -o IITSIS(BSSE1101) -Wall");
-getchar();
+getchar();//Wait for 30 second
 system("start IITSIS(BSSE1101).exe");
 return 0;
 }
