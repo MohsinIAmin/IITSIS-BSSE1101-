821dd185a5c1bcced1261dbb9f5400a80819a95c
Mohsin I Amin <48616524+MohsinIAmin@users.noreply.github.com>
2019-04-23 22:16:12 +0600

    Update and rename MO.c to Just_run_it.c

--- Just_run_it.c
+++ Just_run_it.c
@@ -1,10 +1,17 @@
 //Run on Windows
 #include<stdlib.h>
 #include<stdio.h>
+#include<time.h>
 int main(){
-
+int j=10;
 system("gcc calculate_gpa.c data.h function.h input_marks.c input_profile.c main.c marks_input_menu.c menu.c print_cgpa.c print_gpa.c print_result.c search_1.c search_2.c search_menu.c searching.c show_raw_data.c subject_name.c update_cgpa.c -o IITSIS(BSSE1101) -Wall");
-getchar();//Wait for 30 second
+unsigned int retTime = time(0) + 10;
+    while (time(0) < retTime){
+		if(time(0)==(retTime-j)){
+			printf("Please Waits:%dseconds\n",j);
+			j--;
+		}
+    }
 system("start IITSIS(BSSE1101).exe");
 return 0;
 }
